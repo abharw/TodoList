81d070bfd408fb943f1c6e73d05fd3160dafffcc
Arav Bhardwaj <aravb09@gmail.com>
2024-07-12 14:44:01 -0400

    Initial Commit

---
+++ TodoList/TodoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoListApp.swift
+//  TodoList
+//
+//  Created by Arav Bhardwaj on 7/12/24.
+//
+
+import SwiftUI
+
+@main
+struct TodoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
